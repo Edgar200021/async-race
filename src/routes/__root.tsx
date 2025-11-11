bbe9b567a5c63209d1e3c3c08518d03b003bbe81
Edgar <edgarartur2000@mail.ru>
2025-11-11 16:27:30 +0400

    refactor: remove tanstack router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,11 @@
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { Header } from '@/components/Header'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { Header } from "@/components/Header";
 
 export const Route = createRootRoute({
-  component: () => (
-    <div className="max-w-[1740px] w-full mx-auto px-5 py-4">
-      <Header />
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </div>
-  ),
-})
+	component: () => (
+		<div className="max-w-[1740px] w-full mx-auto px-5 py-4">
+			<Header />
+			<Outlet />
+		</div>
+	),
+});
